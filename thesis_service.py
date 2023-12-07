7a85992c585dc57165b415b4771b5b3eb1ef8fc9
sondonghup <sondonghup@naver.com>
2023-12-07 16:41:37 +0900

    delete bookmark

--- thesis_service.py
+++ thesis_service.py
@@ -35,57 +35,58 @@ search_text = st.text_input('')
 if not search_text :
     st.write('please input')
 
-if search_text != '':
+if search_text:
+
     titles, urls, abstracts, dates, authors = crawl(search_text, search_size, sort_type)
 
 # col1, col2 = st.columns(2)
 
-st.session_state['bookmark'] = []
+    st.session_state['bookmark'] = []
 
-@st.cache_data
-def book_mark_cache(data):
-    return st.session_state['bookmark'].append(data)
+    @st.cache_data
+    def book_mark_cache(data):
+        return st.session_state['bookmark'].append(data)
 
-count = 0
-for title, url, abstract, date, author in zip(titles, urls, abstracts, dates, authors):
+    count = 0
+    for title, url, abstract, date, author in zip(titles, urls, abstracts, dates, authors):
 
-    tab1, tab2 = st.tabs(['abs', 'trans'])
+        tab1, tab2 = st.tabs(['abs', 'trans'])
 
-    with tab1:
-        st.header(f'{title}\n')
-        abstracted, _ = lang.preprocess(abstract)
-        
-        st.info(abstracted)
-        st.info(date)
-        st.info(', '.join(author))
+        with tab1:
+            st.header(f'{title}\n')
+            abstracted, _ = lang.preprocess(abstract)
+            
+            st.info(abstracted)
+            st.info(date)
+            st.info(', '.join(author))
 
-        b1, b2, b3 = st.columns([1, 1, 1])
-        # translate_check = b1.button('translate', key = str(count))
-        bookmark_check = b2.button('bookmark', key = str(count)+'|'+str(count))
-        download_check = b3.button('download', key = str(count)+'|'+str(count)+'|'+str(count))
+            b1, b2, b3 = st.columns([1, 1, 1])
+            # translate_check = b1.button('translate', key = str(count))
+            bookmark_check = b2.button('bookmark', key = str(count)+'|'+str(count))
+            download_check = b3.button('download', key = str(count)+'|'+str(count)+'|'+str(count))
 
-        st.markdown('---')
+            st.markdown('---')
 
-    with tab2:
-        st.header(f'{title}\n')
-        with st.spinner('wait'):
-            translated_title = title
-            transalted_abstracted = lang.translate_func(abstracted)
-        st.success(transalted_abstracted)
+        with tab2:
+            st.header(f'{title}\n')
+            with st.spinner('wait'):
+                translated_title = title
+                transalted_abstracted = lang.translate_func(abstracted)
+            st.success(transalted_abstracted)
 
-        if bookmark_check:
-            st.session_state['bookmark'] = book_mark_cache(count)
-            print(st.session_state['bookmark'])
+            if bookmark_check:
+                st.session_state['bookmark'] = book_mark_cache(count)
+                print(st.session_state['bookmark'])
 
-        st.markdown('---')
-    count += 1
+            st.markdown('---')
+        count += 1
 
-# with col2:
-#     print('here')
-#     st.header('Book Mark')
-#     # if lang.translate_func:
-#     #     print('here')
-#     #     st.header(translated_title)
-#     #     st.error(transalted_abstracted)
-# except Exception as e:
-# print(e)
+    # with col2:
+    #     print('here')
+    #     st.header('Book Mark')
+    #     # if lang.translate_func:
+    #     #     print('here')
+    #     #     st.header(translated_title)
+    #     #     st.error(transalted_abstracted)
+    # except Exception as e:
+    # print(e)
